0960561ba6216b496ff223711065a17e3b04a894
Kauan Souza <kauanoliveiradesouza2016@gmail.com>
2024-10-09 22:18:40 -0300

    v1.1

---
+++ ImpostoDeRenda/src/main/java/kauan/impostoderenda/ImpostoDeRenda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kauan.impostoderenda;
+
+/**
+ *
+ * @author Kauan
+ */
+public class ImpostoDeRenda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
